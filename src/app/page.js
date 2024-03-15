43af14bffd7bbcc9312e235d62351799cf2fcd3d
E-paiva <141731995+E-paiva@users.noreply.github.com>
2024-03-15 20:40:12 +0000

    add components and styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Cores } from './components/Cores'
+import Navbar from './components/NavBar'
+import FooterComponent from './components/footer'
 
 export default function Home() {
   return (
     <main className={styles.main}>
     
-        
+        <Navbar/>
         <Cores/>
-      
+        <FooterComponent/>
 
 
     </main>
