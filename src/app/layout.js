560f91ba676f07c69ec2018a3ea8d1f68cc4f741
Victor Rabelo <vrabeloqa@gmail.com>
2024-03-18 17:54:16 +0000

    Add SEO Metatags

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,20 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Colorista',
-  description: 'o maior da sua aldeia!',
-}
+  title: 'Colorista - The easiest color palette generator on internet',  
+  description: 'The easiest way to generate beautiful colors combinations for your designs.',
+  keywords: ['color scheme', 'scheme color', 'color schemes', 'color theme', 'color palette',
+             'color themes', 'color palettes', 'color combinations', 'color', 'colors', 'palettes',
+             'hex color wheel', 'pastel color palette', 'color wheel', 'color combination', 'color pallete',
+             'color names', 'web color schemes', 'color wheel generator', 'random color palette generator',
+             'color schemes generator', 'ui color palette generator', 'pretty colors', 'vintage color'],
+  
+  icons: {
+    icon: './favicon.svg',
+  },
+
+  }           
+
 
 export default function RootLayout({ children }) {
   return (
